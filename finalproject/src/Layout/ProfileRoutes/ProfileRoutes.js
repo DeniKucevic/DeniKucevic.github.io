b076eb95830742c88df528d9fdb114dc17177903
Denis Kucevic <denikucevic@gmail.com>
2020-04-03 04:45:10 +0200

    io test

--- finalproject/src/Layout/ProfileRoutes/ProfileRoutes.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Route } from "react-router-dom";
-
-const ProfileRoutes = ({ component: Component, ...rest }) => {
-  return <Route>{<Component {...rest} />}</Route>;
-};
-
-export default ProfileRoutes;
