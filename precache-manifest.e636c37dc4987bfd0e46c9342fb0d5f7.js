ade443dbd89a83dd7f56f14398a2f0bfce3d2452
Denis Kucevic <denikucevic@gmail.com>
2020-04-03 04:40:49 +0200

    io test

---
+++ precache-manifest.e636c37dc4987bfd0e46c9342fb0d5f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e18ce546ca4fe0facfbd0e4a6a845e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d15ee34d2648d5d086d",
+    "url": "/static/css/main.7239b272.chunk.css"
+  },
+  {
+    "revision": "d3591792ba7ebd9cc6c5",
+    "url": "/static/js/2.ecc0f9bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ecc0f9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d15ee34d2648d5d086d",
+    "url": "/static/js/main.61c053b0.chunk.js"
+  },
+  {
+    "revision": "e9a514e947ab475640c5",
+    "url": "/static/js/runtime-main.40de6a82.js"
+  }
+]);
